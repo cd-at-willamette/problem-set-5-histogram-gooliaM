767d320f63e418219c61ba4cccf17c030acada98
gooliaM <jamartin@willamette.edu>
2024-10-27 12:39:36 -0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -29,18 +29,14 @@ def graph_histogram(hist:list[int], width:int, height:int) -> None:
 
     gw = GWindow(width, height)
 
-    max = 0
-    for i in range(len(hist)):
-        for j in range(len(hist[i])):  
-            if hist[i][j] > max: 
-                max = hist[i][j]
+    #written by chatgpt
+    max_count = max(len(group) for group in hist)
 
     col_width = width / len(hist)
     
     for i in range(len(hist)): #This code was made by chatgpt
-        
         count = len(hist[i])  # Get the count of occurrences for this index
-        rect_height = (height / max) * count  # Scale height based on the count
+        rect_height = (height / max_count) * count  # Scale height based on the count
         x = col_width * i  # Calculate x position for each column
         y = height - rect_height  # Position from the bottom
 
@@ -55,4 +51,3 @@ hist = create_histogram_array(PI_DIGITS)
 print(hist)
 print_histogram(hist) # uncomment to test
 graph_histogram(hist, 400, 400) # uncomment to test
-
